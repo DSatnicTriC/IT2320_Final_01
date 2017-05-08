f9c06ab498b312ff915d12c66989f93ec2daabfe
achoo54321 <actionnotmotion@gmail.com>
2017-05-08 17:55:29 -0400

    The login and create validation is now completely functional and matches the service tech specs doc

--- FinalProject/Scripts/Home.js
+++ FinalProject/Scripts/Home.js
@@ -34,7 +34,7 @@
             $.ajax({
                 url: window.FinalProjectUrl_CreateAccount,
                 data: dataObject,
-                success: successAccountCreate,
+                success: newUserCreateButtonProcessor,
                 dataType: "json"
             });
         }
@@ -52,6 +52,7 @@ function hideMessages() {
 }
 
 function validateLoginInputs() {
+    //dsatnic 2017-05-08: I need to handle here because the server does not check for nulls in the request
     if ($("#existingUserUserName").val().trim() === "") {
         $("#existingUserUserNameMessage").html("Please enter a user name");
         $("#existingUserUserNameMessage").show();
@@ -66,24 +67,10 @@ function validateLoginInputs() {
 }
 
 function validateCreationInputs() {
-    if ($("#newUserUserName").val().trim() === "") {
-        $("#newUserUserNameMessage").html("Please enter a user name");
-        $("#newUserUserNameMessage").show();
-        return false;
-    }
-    if ($("#newUserPassword").val().trim() === "") {
-        $("#newUserPasswordMessage").html("Please enter a password");
-        $("#newUserPasswordMessage").show();
-        return false;
-    }
-    if ($("#newUserEmail").val().trim() === "") {
+    //dsatnic 2017-05-08: I need to handle this possibility because this is not handled server side and results in a 500 error
+    if ($("#newUserEmail").val().trim() === "" && $("#newUserEmailRepeat").val().trim() !== "") {
         $("#newUserEmailMessage").html("Please enter an email address");
         $("#newUserEmailMessage").show();
-        return false;
-    }
-    if ($("#newUserEmailRepeat").val().trim() === "") {
-        $("#newUserEmailRepeatMessage").html("Please re-enter the email address");
-        $("#newUserEmailRepeatMessage").show();
         return false;
     }
     return true;
@@ -96,15 +83,62 @@ function existingUserLogInButtonProcessor(responseData) {
 
     } else {
         if (responseData.Username === "Invalid") {
-            $("#existingUserUserNameMessage").html("The username you entered is invalid");
+            $("#existingUserUserNameMessage").html("Must be an existing account username");
             $("#existingUserUserNameMessage").show();
         }
         else if (responseData.Password === "Wrong") {
-            $("#existingUserPasswordMessage").html("The password you entered is wrong");
+            $("#existingUserPasswordMessage").html("Wrong password for existing account");
             $("#existingUserPasswordMessage").show();
         }
     }
 
+}
+
+function newUserCreateButtonProcessor(responseData) {
+    // dsatnic 2017-05-08: not sure why the ajax is not auto parsing the JSON; however, this works
+    responseData = jQuery.parseJSON(responseData);
+    if (responseData.Message === "Success") {
+
+    } else {
+        if (responseData.Username !== "Good") {
+            if (responseData.Username === "Invalid") {
+                $("#newUserUserNameMessage").html("Must be at least 6 characters");
+            }
+            else if (responseData.Username === "Exists") {
+                $("#newUserUserNameMessage").html("Username is already taken");
+            } else {
+                $("#newUserUserNameMessage").html(responseData.Username);
+            }
+            $("#newUserUserNameMessage").show();
+        }
+        if (responseData.Password !== "Good") {
+            if (responseData.Password === "Invalid") {
+                $("#newUserPasswordMessage").html("Must be at least 6 characters");
+            } else {
+                $("#newUserPasswordMessage").html(responseData.Password);
+            }
+            $("#newUserPasswordMessage").show();
+        }
+        if (responseData.EmailAdd !== "Good") {
+            if (responseData.EmailAdd === "Invalid") {
+                $("#newUserEmailMessage").html("Must have a value and contain \"@\"");
+            } else {
+                $("#newUserEmailMessage").html(responseData.EmailAdd);
+            }
+            $("#newUserEmailMessage").show();
+        }
+        if (responseData.EmailCon !== "Good") {
+            if (responseData.EmailCon === "Invalid") {
+                $("#newUserEmailRepeatMessage").html("Must have a value");
+            }
+            else if (responseData.EmailCon === "Mismatch") {
+                $("#newUserEmailRepeatMessage").html("Must match EmailAdd");
+            } else {
+                $("#newUserEmailRepeatMessage").html(responseData.EmailCon);
+            }
+            $("#newUserEmailRepeatMessage").show();
+        }
+    }
 }
 
 function successAccountCreate(responseData) {
